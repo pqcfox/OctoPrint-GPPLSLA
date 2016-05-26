61ddc72864d7cd1c9f3467c6aeafb800a7585b6e
watsonforprez <prezwatson2016@gmail.com>
2016-05-25 21:43:23 -0700

    Implement base of G420 parser

--- octoprint_gpplsla/__init__.py
+++ octoprint_gpplsla/__init__.py
@@ -1,72 +1,50 @@
 # coding=utf-8
 from __future__ import absolute_import
 
-### (Don't forget to remove me)
-# This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
-# as well as the plugin mixins it's subclassing from. This is really just a basic skeleton to get you started,
-# defining your plugin as a template plugin, settings and asset plugin. Feel free to add or remove mixins
-# as necessary.
-#
-# Take a look at the documentation on what other plugin mixins are available.
+import re
+from Tkinter import *
 
 import octoprint.plugin
 
-class GpplslaPlugin(octoprint.plugin.SettingsPlugin,
-                    octoprint.plugin.AssetPlugin,
-                    octoprint.plugin.TemplatePlugin):
+class GPPLSLAPlugin(octoprint.plugin.StartupPlugin):
+    def on_after_startup(self):
+        self._logger.info("Hello World!")
 
-	##~~ SettingsPlugin mixin
+    def sending_g420(self, comm_instance, phase, cmd, cmd_type, gcode, *args, **kwargs):
+        if gcode and gcode == "G420":  # stop laughing
+            self._logger.info("Sending G420: {}".format(cmd))
+            image_name = re.search(r"P(\S+)", cmd).group(1)
+            image_path = self.get_image_path(image_name)
+            pause = int(re.search(r"P(\d+)", cmd).group(1))
+            self.display_image(image_path, pause)
+            
+    @static_method
+    def get_image_path(image_name):
+        pass
+            
+    @static_method
+    def render_svg():
+        pass
 
-	def get_settings_defaults(self):
-		return dict(
-			# put your plugin's default settings here
-		)
+    @static_method
+    def display_image(image_path, pause):
+        root = Tk()
+        width = root.winfo_screenwidth()
+        height = root.winfo_screenheight()
+        root.overrideredirect(1)
+        root.geometry("{}x{}+0+0".format(width, height))
+        root.focus_set()
+        root.after(pause * 1000, lambda: root.destroy())
+        root.mainloop()
+        
 
-	##~~ AssetPlugin mixin
-
-	def get_assets(self):
-		# Define your plugin's asset files to automatically include in the
-		# core UI here.
-		return dict(
-			js=["js/gpplsla.js"],
-			css=["css/gpplsla.css"],
-			less=["less/gpplsla.less"]
-		)
-
-	##~~ Softwareupdate hook
-
-	def get_update_information(self):
-		# Define the configuration for your plugin to use with the Software Update
-		# Plugin here. See https://github.com/foosel/OctoPrint/wiki/Plugin:-Software-Update
-		# for details.
-		return dict(
-			gpplsla=dict(
-				displayName="Gpplsla Plugin",
-				displayVersion=self._plugin_version,
-
-				# version check: github repository
-				type="github_release",
-				user="useanalias",
-				repo="OctoPrint-GPPLSLA",
-				current=self._plugin_version,
-
-				# update method: pip
-				pip="https://github.com/useanalias/OctoPrint-GPPLSLA/archive/{target_version}.zip"
-			)
-		)
-
-
-# If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
-# ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
-# can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
-__plugin_name__ = "Gpplsla Plugin"
+__plugin_name__ = "GPPL SLA"
 
 def __plugin_load__():
-	global __plugin_implementation__
-	__plugin_implementation__ = GpplslaPlugin()
+    plugin = GPPLSLAPlugin()
 
-	global __plugin_hooks__
-	__plugin_hooks__ = {
-		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
-	}
+    global __plugin_implementation__
+    __plugin_implementation__ = plugin
 
+    global __plugin_hooks__
+    __plugin_hooks__ = {"octoprint.comm.protocol.gcode.sending": plugin.sending_g420}
