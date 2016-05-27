7d0ec67390540c70431abd2f57322e1327201cbe
watsonforprez <prezwatson2016@gmail.com>
2016-05-26 20:54:33 -0700

    Remove G419 call

--- octoprint_gpplsla/__init__.py
+++ octoprint_gpplsla/__init__.py
@@ -7,62 +7,31 @@ from Tkinter import *
 
 import octoprint.plugin
 
-class GPPLSLAPlugin(octoprint.plugin.EventHandlerPlugin,
-                    octoprint.plugin.SettingsPlugin, 
-                    octoprint.plugin.StartupPlugin,
-                    octoprint.plugin.TemplatePlugin):
-    def on_after_startup(self):
-        self._logger.info("Hello World! image_archive = {}".format(self._settings.get(["image_archive"])))
+payload = None
 
-    def get_settings_defaults(self):
-        return dict(image_archive="images.tar.gz")
+def sending_g420(self, comm_instance, phase, cmd, cmd_type, gcode, *args, **kwargs):
+    if gcode:
+        if gcode == "M654":
+            self._logger.info("Handling M654: {}".format(cmd))
+        if gcode == "G420":  # stop laughing
+            self._logger.info("Handling G420: {}".format(cmd))
+            self.display_image(self.render_svg(self.decode_svg(payload)))
 
-    def get_template_configs(self):
-        return [dict(type="settings", custom_bindings=False)]
+@staticmethod
+def decode_svg(text):
+    pass
 
-    def sending_g419_g420(self, comm_instance, phase, cmd, cmd_type, gcode, *args, **kwargs):
-        if gcode:
-            if gcode == "G419":
-                self._logger.info("Sending G419: {}".format(cmd))
-            if gcode == "G420":  # stop laughing
-                self._logger.info("Sending G420: {}".format(cmd))
-                image_name = re.search(r"P(\S+)", cmd).group(1)
-                image_path = self.get_image_path(image_name)
-                pause = int(re.search(r"P(\d+)", cmd).group(1))
-                self.display_image(image_path, pause)
-
-    def on_event(self, event, payload):
-        if event == "PrintStarted":
-            archive = self._settings.get(["image_archive"])
-            
-
-    @staticmethod
-    def get_image_path(image_name):
-        pass
-            
-    @staticmethod
-    def render_svg():
-        pass
-
-    @staticmethod
-    def display_image(image_path, pause):
-        root = Tk()
-        width = root.winfo_screenwidth()
-        height = root.winfo_screenheight()
-        root.overrideredirect(1)
-        root.geometry("{}x{}+0+0".format(width, height))
-        root.focus_set()
-        root.after(pause * 1000, lambda: root.destroy())
-        root.mainloop()
+@staticmethod
+def display_image(image_path, pause):
+    root = Tk()
+    width = root.winfo_screenwidth()
+    height = root.winfo_screenheight()
+    root.overrideredirect(1)
+    root.geometry("{}x{}+0+0".format(width, height))
+    root.focus_set()
+    root.after(pause * 1000, lambda: root.destroy())
+    root.mainloop()
         
 
 __plugin_name__ = "GPPL SLA"
-
-def __plugin_load__():
-    plugin = GPPLSLAPlugin()
-
-    global __plugin_implementation__
-    __plugin_implementation__ = plugin
-
-    global __plugin_hooks__
-    __plugin_hooks__ = {"octoprint.comm.protocol.gcode.sending": plugin.sending_g419_g420}
+__plugin_hooks__ = {"octoprint.comm.protocol.gcode.sending": plugin.sending_g420}
