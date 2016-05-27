28954eab402218ba4a6bffdbc14f22e0ae0b3a22
watsonforprez <prezwatson2016@gmail.com>
2016-05-27 11:35:10 -0700

    Add SVG rendering code

--- octoprint_gpplsla/__init__.py
+++ octoprint_gpplsla/__init__.py
@@ -1,37 +1,65 @@
 # coding=utf-8
 from __future__ import absolute_import
 
+import base64
+import cairo
 import re
-import tarfile
+import rsvg
+from cStringIO import StringIO
+from PIL import Image
 from Tkinter import *
 
 import octoprint.plugin
 
-payload = None
+class GPPLSLAPlugin(octoprint.plugin.SettingsPlugin,
+                    octoprint.plugin.TemplatePlugin):
+    def get_settings_defaults(self):		
+        return dict(scale_factor=8)
 
-def sending_g420(self, comm_instance, phase, cmd, cmd_type, gcode, *args, **kwargs):
-    if gcode:
-        if gcode == "M654":
-            self._logger.info("Handling M654: {}".format(cmd))
-        if gcode == "G420":  # stop laughing
-            self._logger.info("Handling G420: {}".format(cmd))
-            self.display_image(self.render_svg(self.decode_svg(payload)))
+    def get_template_configs(self):
+        return [dict(type="settings", custom_bindings=False)]
 
-@staticmethod
-def decode_svg(text):
-    pass
+    def sending_g420(self, comm_instance, phase, cmd, cmd_type, gcode, *args, **kwargs):
+        if gcode:
+            if gcode == "M654":
+                self._logger.info("Handling M654: {}".format(cmd))
+                payload = re.search(r"\sE(\S+)", cmd).group(1)
+            if gcode == "G420":  # stop laughing
+                self._logger.info("Handling G420: {}".format(cmd))
+                time = float(re.search(r"\sP(\S+)", cmd).group(1))
+                display_payload(payload, time)
 
-@staticmethod
-def display_image(image_path, pause):
-    root = Tk()
-    width = root.winfo_screenwidth()
-    height = root.winfo_screenheight()
-    root.overrideredirect(1)
-    root.geometry("{}x{}+0+0".format(width, height))
-    root.focus_set()
-    root.after(pause * 1000, lambda: root.destroy())
-    root.mainloop()
+    @staticmethod
+    def render_svg(payload, size, root):
+        image = cairo.ImageSurface(cairo.FORMAT_ARGB32, *size)
+        context = cairo.Context(image)
+        handle = rsvg.Handle(None, base64.b64decode(payload))
+        handle.render_cairo(context)
+        return Image.open(StringIO(img.get_data()))
+
+    @staticmethod
+    def display_image(payload, pause):
+        root = Tk()
+        width = root.winfo_screenwidth()
+        height = root.winfo_screenheight()
+        size = (width, height)
+        image = render_svg(payload, size, root)
+        panel = Label(root, image=image)
+        panel.pack(side="bottom", fill="both", expand="yes")
+        root.overrideredirect(1)
+        root.geometry("{}x{}+0+0".format(*size))
+        root.focus_set()
+        root.after(pause * 1000, lambda: root.destroy())
+        root.mainloop()
         
 
 __plugin_name__ = "GPPL SLA"
-__plugin_hooks__ = {"octoprint.comm.protocol.gcode.sending": plugin.sending_g420}
+
+def __plugin_load__():
+    plugin = GPPLSLA()
+
+    global __plugin_implementation__
+    __plugin_implementation__ = plugin
+
+    global __plugin_hooks__
+    __plugin_hooks__ = {"octoprint.comm.protocol.gcode.sending": plugin.sending_g420}
